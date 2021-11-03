77ec16527ffae1c6ad2766887209db8a0f097385
Jason Carneiro <jasondavin.dev@gmail.com>
2021-11-03 19:59:03 -0300

    Finish apriori analysis and add apyori lib (#3)
    * wup
    
    * rename module
    
    * move count line
    
    * add prune
    
    * remove frequency global variable
    
    * remove unnecessary code
    
    * finish apriori
    
    * remove print
    
    * add apyori lib

--- apriori.py
+++ apriori.py
@@ -1,95 +1,144 @@
-from itertools import combinations, product
+import argparse
 
-from domain.entities.product import Product
-from domain.entities.transaction import Transaction
+from apyori import apriori
+
 from domain.repositories.product_repository import get_all_products
-from domain.repositories.transaction_repository import get_all_transactions
+from domain.repositories.transaction_items_repository import \
+    get_all_transaction_items
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--period-day', required=False)
+parser.add_argument('--min-sup', required=True, type=float)
+parser.add_argument('--min-conf', required=True, type=float)
+args = parser.parse_args()
+
+MIN_SUP = args.min_sup
+MIN_CONF = args.min_conf
+PERIOD_DAY = args.period_day
 
 
-def get_strongest_associations():
+def prune(rules, min_sup, min_conf):
+    return [rule for rule in rules if rule['support'] >= min_sup and rule['confidence'] >= min_conf]
+
+
+def get_strongest_associations(min_sup, min_conf, period_day=None):
     associations = {}
 
-    transactions = get_all_transactions()
+    transactions = get_all_transaction_items(period_day=period_day)
     products = get_all_products()
 
-    combinations_of_products = list(combinations(products, 2))
+    support_list = []
+    confidence_list = []
 
-    count = 0
-    max = len(combinations_of_products)
+    for product in products:
+        support_value = support(
+            product.product_id, product.product_id, transactions)
 
-    print(f"Calculating associations for {max} combinations")
-    print("===================================")
-    for combination in combinations_of_products:
-        first_item = combination[0]
-        second_item = combination[1]
-        support_value = support(first_item, second_item, transactions)
-        confidence_value = confidence(first_item, second_item, transactions)
-
-        associations[(first_item.product_name, second_item.product_name)] = {
+        support_list.append({
             "support": support_value,
-            "confidence": confidence_value
-        }
+            "confidence": 1,
+            "rule": str(product.product_id),
+            "product_name": product.product_name
+        })
 
-        print(
-            f"combination: {first_item.product_name} - {second_item.product_name}")
-        print(f"support: {support_value}")
-        print(f"confidence: {confidence_value}")
-        print(f"{count} / {max}")
-        print("===================================")
+    sup_prune_list = prune(support_list, min_sup, 1)
 
-        count += 1
+    for item1 in sup_prune_list:
+        for item2 in sup_prune_list:
+            if item1['rule'] != item2['rule']:
+                rule = f"{item1['rule']}_{item2['rule']}"
+                support_value = support(
+                    int(item1['rule']), int(item2['rule']), transactions)
+                confidence_value = confidence(
+                    int(item1['rule']), int(item2['rule']), transactions)
 
-    associations = sorted(associations.items(),
-                          key=lambda x: x[1]["confidence"])
-    return [associations.pop(), associations.pop()]
+                confidence_list.append({
+                    "support": support_value,
+                    "confidence": confidence_value,
+                    "rule": rule,
+                    "combination": f"{item1['product_name']} - {item2['product_name']}"
+                })
+
+    associations = prune(confidence_list, min_sup, min_conf)
+    associations = sorted(associations,
+                          key=lambda x: (x["confidence"], x['support']))
+
+    return associations[-2:]
 
 
-def support(first_item: Product, second_item: Product, transactions: list[Transaction]):
+def count_frequence(transactions: dict[int, set[int]], itemset: set[int]):
+    count = 0
+
+    for items in transactions.values():
+        if itemset.issubset(items):
+            count += 1
+
+    return count
+
+
+def support(first_item: int, second_item: int, transactions: dict[int, set[int]]):
     """
     Returns the support of an itemset in a list of transactions.
     """
 
-    itemset = set([first_item.product_id, second_item.product_id])
-    return len(
-        [
-            1 for transaction in transactions if itemset.issubset(
-                set([item.product_id for item in transaction.transaction_items])
-            )
-        ]
-    ) / len(transactions)
+    itemset = set([first_item, second_item])
+    return count_frequence(transactions, itemset) / len(transactions.keys())
 
 
-def confidence(first_item: Product, second_item: Product, transactions: list[Transaction]):
+def confidence(first_item: int, second_item: int, transactions: dict[int, set[int]]):
     """
     Returns the confidence of an itemset in a list of transactions.
     """
-    itemset = set([first_item.product_id, second_item.product_id])
-    return len(
-        [
-            1 for transaction in transactions if itemset.issubset(
-                set([item.product_id for item in transaction.transaction_items])
-            )
-        ]
-    ) / len(
-        [
-            1 for transaction in transactions if first_item.product_id in
-            [item.product_id for item in transaction.transaction_items]
-        ]
-    )
+    itemset = set([first_item, second_item])
 
+    count = 0
 
-def get_transaction_product_ids(transactions: list[Transaction]):
-    transaction_product_ids = []
-    for transaction in transactions:
-        transaction_product_ids.append(
-            [item.product_id for item in transaction.transaction_items])
-    return transaction_product_ids
+    for transaction_items in transactions.values():
+        if first_item in transaction_items:
+            count += 1
+
+    return count_frequence(transactions, itemset) / count
 
 
 def main():
     if __name__ == "__main__":
-        strongest_associations = get_strongest_associations()
+        print("ALL TRANSACTIONS")
+        strongest_associations = get_strongest_associations(MIN_SUP, MIN_CONF)
         print(strongest_associations)
 
+        print("MORNING TRANSACTIONS")
+        strongest_associations_morning = get_strongest_associations(
+            MIN_SUP, MIN_CONF, 'morning')
+        print(strongest_associations_morning)
+
+        print("AFTERNOON TRANSACTIONS")
+        strongest_associations_afternoon = get_strongest_associations(
+            MIN_SUP, MIN_CONF, 'afternoon')
+        print(strongest_associations_afternoon)
+
+        print("EVENING TRANSACTIONS")
+        strongest_associations_evening = get_strongest_associations(
+            MIN_SUP, MIN_CONF, 'evening')
+        print(strongest_associations_evening)
+
+        all_transactions = [list(e[1]) for e in get_all_transaction_items().items()]
+        morning_transactions = [list(e[1]) for e in get_all_transaction_items('morning').items()]
+        afternoon_transactions = [list(e[1]) for e in get_all_transaction_items('afternoon').items()]
+        evening_transactions = [list(e[1]) for e in get_all_transaction_items('evening').items()]
+
+        print('ALL TRANSACTIONS')
+        print(list(apriori(all_transactions, min_support=MIN_SUP, min_confidence=MIN_CONF)))
+        print('MORNING TRANSACTIONS')
+        print(list(apriori(morning_transactions, min_support=MIN_SUP, min_confidence=MIN_CONF)))
+        print('AFTERNOON TRANSACTIONS')
+        print(list(apriori(afternoon_transactions, min_support=MIN_SUP, min_confidence=MIN_CONF)))
+        print('EVENING TRANSACTIONS')
+        print(list(apriori(evening_transactions, min_support=MIN_SUP, min_confidence=MIN_CONF)))
+
+        # LIFT PARAM
+        # Lift basically tells us that the likelihood of buying a Burger and Ketchup
+        # together is 3.33 times more than the likelihood of just buying the ketchup.
+        # A Lift of 1 means there is no association between products A and B.
+        # Lift of greater than 1 means products A and B are more likely to be bought together.
 
 main()
