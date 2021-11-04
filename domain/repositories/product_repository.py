b929736eeacc18a4550ca11d1567aab531076e93
Jason Carneiro <jasondavin.dev@gmail.com>
2021-11-04 19:09:57 -0300

    update and use repositories (#5)

--- domain/repositories/product_repository.py
+++ domain/repositories/product_repository.py
@@ -1,5 +1,16 @@
 from domain.db.config import session
 from domain.entities.product import Product
 
+
 def get_all_products() -> list[Product]:
-  return session.query(Product).all()
+    return session.query(Product).all()
+
+
+def get_product_by_name(name: str):
+    return session.query(Product).filter_by(product_name=name).first()
+
+
+def create_product(product_name: str):
+    product = Product(product_name)
+    session.add(product)
+    return product
